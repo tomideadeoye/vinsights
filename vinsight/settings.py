92331677027c390419080fe74d1b967f33d12fd3
Tomide Adeoye <55337742+tomideadeoye@users.noreply.github.com>
2023-02-27 14:03:50 +0100

    static collect

--- vinsight/settings.py
+++ vinsight/settings.py
@@ -14,7 +14,9 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
