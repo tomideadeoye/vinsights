064770fb3e444b2dc1b1d3fdb26e8bace8688196
Tomide Adeoye <tommideadeoye@gmail.com>
2023-03-01 14:40:26 +0100

    cors issue

--- vinsight/settings.py
+++ vinsight/settings.py
@@ -40,14 +40,22 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:8000',
-    'https://king-prawn-app-c9gz3.ondigitalocean.app',
-    "*"
-    "localhost:3000",
+# CORS_ORIGIN_ALLOW_ALL = False
+# CORS_ORIGIN_WHITELIST = (
+#     'http://localhost:8000',
+#     'https://king-prawn-app-c9gz3.ondigitalocean.app',
+#     "*",
+#     "localhost:3000",
 
-)
+
+# )
+
+CORS_ALLOWED_ORIGINS = ['http://localhost:3001',
+                        'https://king-prawn-app-c9gz3.ondigitalocean.app',
+                        'http://localhost:3000',
+                        'http://192.168.1.125:3000'
+                        ]
+
 
 ROOT_URLCONF = 'vinsight.urls'
 
