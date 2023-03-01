915532156e4f2ba6d86d6456580241b1e252aa7d
Tomide Adeoye <tommideadeoye@gmail.com>
2023-03-01 14:50:10 +0100

    pitch uploaded fix

--- memogenerator/models.py
+++ memogenerator/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Company(models.Model):
     name = models.CharField(max_length=100)
     website = models.CharField(max_length=100)
-    pitch = models.CharField(max_length=100)
+    pitch_uploaded = models.FileField(
+        upload_to='mediafiles/', null=True, blank=True)
+    emailTo = models.CharField(max_length=100, null=True, blank=True
+                               )
+
     def __str__(self):
         return self.name
